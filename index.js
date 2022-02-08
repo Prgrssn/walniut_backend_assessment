93607c000277635ce5b2cbac1532b80c0116fd79
Joshua Young <prgrssn@gmail.com>
2022-02-08 14:22:54 -0800

    create a json file to store responses

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get("/post", (_req, res) => {
+  app.get(
+    "https://api.hatchways.io/assessment/blog/posts?tag=tech",
+    (_req, res) => {
+      res.json(res);
+    }
+  );
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
