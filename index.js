0f417748ab8aad7efe6bddb44a1d1c32469344bc
Joshua Young <prgrssn@gmail.com>
2022-02-08 15:24:20 -0800

    make progress on getting data into json file

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-var express = require("express");
-var app = express();
+const fs = require("fs");
+const express = require("express");
+const app = express();
 
-app.get("/post", (_req, res) => {
-  app.get(
-    "https://api.hatchways.io/assessment/blog/posts?tag=tech",
-    (_req, res) => {
-      res.json(res);
-    }
-  );
+let posts = [];
+const tags = ["tech", "science", "health", "history"];
+
+const promises = tags.map((tag) => {
+  app.get(`https://api.hatchways.io/assessment/blog/posts?${tag}`);
+});
+
+Promise.all(promises)
+  .then(function (posts) {
+    posts.forEach((post) => {
+      console.log(post);
+      // posts.push(post);
+      // fs.writeFile("./data/data.json", JSON.stringify(posts), (err) => {
+      //   if (err) {
+      //     console.log(err);
+      //     return;
+      //   } else {
+      //     console.log("success!");
+      //     return;
+      //   }
+      // });
+    });
+  })
+  .catch((err) => console.log(err));
+
+// tags.map((tag) => {
+//   console.log(tag);
+//   app.get(
+//     `https://api.hatchways.io/assessment/blog/posts?${tag}`,
+//     (req, res) => {
+//       let fetchedPosts = res.posts;
+//       console.log(fetchedPosts);
+//         posts.push(fetchedPosts);
+// fs.writeFile("./data/data.json", JSON.stringify(posts), (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("success!");
+//   }
+// });
+//     }
+//   );
+// });
+
+// console.log(posts);
+
+app.get("/api/ping", (_req, res) => {
+  res.status(200).json({ success: true });
 });
 
 app.listen(8080, () => {
