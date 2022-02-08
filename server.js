7ade1b25636ac1870a8458771a6cc6737b8dffb0
Joshua Young <prgrssn@gmail.com>
2022-02-08 13:51:58 -0800

    add files

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 
 API_URL = `http://localhost:${PORT}`;
 
+app.get(
+  "https://api.hatchways.io/assessment/blog/posts?tag=tech",
+  (req, res) => {
+    res.json(res.body);
+  }
+);
+
 app.listen(PORT, () => {
   console.log(`Express server running on port: ${PORT}`);
 });
