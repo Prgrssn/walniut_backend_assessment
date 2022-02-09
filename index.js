dcf5155ed862cc1f31c9a63d7796b19ae0c2a95a
Joshua Young <prgrssn@gmail.com>
2022-02-08 16:10:18 -0800

    brainstorm how to get multiple api calls by maping an array

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 const fs = require("fs");
 const express = require("express");
+const { request } = require("http");
+const https = require("https");
+const { callbackify } = require("util");
 const app = express();
 
 let posts = [];
 const tags = ["tech", "science", "health", "history"];
 
-const promises = tags.map((tag) => {
-  app.get(`https://api.hatchways.io/assessment/blog/posts?${tag}`);
-});
+// (async function () {
+//   await Promise.all(
+//     tags.map(async (tag) => {
+//       const response = await fetch(
+// `https://api.hatchways.io/assessment/blog/posts?${tag}`
+//       );
+//       const post = await response.json();
+//       console.log(typeof response);
+//     })
+//   );
+// });
 
-Promise.all(promises)
-  .then(function (posts) {
-    posts.forEach((post) => {
-      console.log(post);
-      // posts.push(post);
-      // fs.writeFile("./data/data.json", JSON.stringify(posts), (err) => {
-      //   if (err) {
-      //     console.log(err);
-      //     return;
-      //   } else {
-      //     console.log("success!");
-      //     return;
-      //   }
-      // });
-    });
-  })
-  .catch((err) => console.log(err));
+function getPosts() {
+  tags.map((tag) => {
+    https.get(
+      `https://api.hatchways.io/assessment/blog/posts?${tag}`,
+      function (err, response, body) {
+        if (err) {
+          console.log(err);
+          callbackify(err);
+        } else {
+          let posts = JSON.parse(body);
+          console.log(posts);
+        }
+      }
+    );
+  });
+}
+
+getPosts();
+
+// Promise.all(promises)
+//   .then(function (posts) {
+//     posts.forEach((post) => {
+//       posts.push(post);
+//       fs.writeFile("./data/data.json", JSON.stringify(posts), (err) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         } else {
+//           console.log("success!");
+//           return;
+//         }
+//       });
+//     });
+//   })
+//   .catch((err) => console.log(err));
 
 // tags.map((tag) => {
 //   console.log(tag);
 //   app.get(
-//     `https://api.hatchways.io/assessment/blog/posts?${tag}`,
+// `https://api.hatchways.io/assessment/blog/posts?${tag}`,
 //     (req, res) => {
 //       let fetchedPosts = res.posts;
 //       console.log(fetchedPosts);
@@ -47,6 +76,11 @@ Promise.all(promises)
 // });
 
 // console.log(posts);
+
+app.get(`https://api.hatchways.io/assessment/blog/posts?tech`, (req, res) => {
+  posts = JSON.stringify(res);
+  console.log(posts);
+});
 
 app.get("/api/ping", (_req, res) => {
   res.status(200).json({ success: true });
